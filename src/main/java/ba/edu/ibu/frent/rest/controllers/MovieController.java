098b366c047e4fc67b6f5f16743b96fbd37b1d02
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-10-12 15:52:52 +0200

    set up movie controller

--- src/main/java/ba/edu/ibu/frent/rest/controllers/MovieController.java
+++ src/main/java/ba/edu/ibu/frent/rest/controllers/MovieController.java
@@ -1,2 +1,29 @@
-package ba.edu.ibu.frent.rest.controllers;public class MovieController {
+package ba.edu.ibu.frent.rest.controllers;
+
+import ba.edu.ibu.frent.core.model.Movie;
+import ba.edu.ibu.frent.core.service.MovieService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/movies")
+public class MovieController {
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping public List<Movie> findAll() {
+        return movieService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Movie findById(@PathVariable int id) {
+        return movieService.findById(id);
+    }
 }
