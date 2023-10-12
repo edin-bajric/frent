0cc85c7a442b4784c4235124cec018285d4dc2e1
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-10-12 15:28:05 +0200

    set up movie model

--- src/main/java/ba/edu/ibu/frent/core/model/Movie.java
+++ src/main/java/ba/edu/ibu/frent/core/model/Movie.java
@@ -1,2 +1,57 @@
-package ba.edu.ibu.frent.core.model;public class Movie {
+package ba.edu.ibu.frent.core.model;
+
+public class Movie {
+    private int id;
+    private String title;
+    private String director;
+    private String genre;
+    private int year;
+
+    public Movie(int id, String title, String director, String genre, int year) {
+        this.id = id;
+        this.title = title;
+        this.director = director;
+        this.genre = genre;
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
