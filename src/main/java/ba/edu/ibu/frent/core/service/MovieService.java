9dccabbe25b0e06d755b7e083e85967a49f6be21
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-10-12 15:45:16 +0200

    set up movie service

--- src/main/java/ba/edu/ibu/frent/core/service/MovieService.java
+++ src/main/java/ba/edu/ibu/frent/core/service/MovieService.java
@@ -1,2 +1,24 @@
-package ba.edu.ibu.frent.core.service;public class MovieService {
+package ba.edu.ibu.frent.core.service;
+
+import ba.edu.ibu.frent.core.model.Movie;
+import ba.edu.ibu.frent.core.repository.MovieRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public List<Movie> findAll() {
+        return movieRepository.findAll();
+    }
+
+    public Movie findById(int id) {
+        return movieRepository.findById(id);
+    }
 }
