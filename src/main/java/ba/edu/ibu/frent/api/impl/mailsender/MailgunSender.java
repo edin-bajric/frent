c69e970dce2921b71fcecc16eb4134750ffb55e2
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-10-14 13:59:36 +0200

    added method 2 for sending emails

--- src/main/java/ba/edu/ibu/frent/api/impl/mailsender/MailgunSender.java
+++ src/main/java/ba/edu/ibu/frent/api/impl/mailsender/MailgunSender.java
@@ -2,11 +2,13 @@ package ba.edu.ibu.frent.api.impl.mailsender;
 
 import ba.edu.ibu.frent.core.api.mailsender.MailSender;
 import ba.edu.ibu.frent.core.model.User;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@ConditionalOnProperty(name = "configuration.mailsender.default", havingValue = "mailgun")
 public class MailgunSender implements MailSender {
     @Override
     public String send(List<User> users, String message) {
--- src/main/java/ba/edu/ibu/frent/core/service/UserService.java
+++ src/main/java/ba/edu/ibu/frent/core/service/UserService.java
@@ -1,7 +1,9 @@
 package ba.edu.ibu.frent.core.service;
 
+import ba.edu.ibu.frent.core.api.mailsender.MailSender;
 import ba.edu.ibu.frent.core.model.User;
 import ba.edu.ibu.frent.core.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,7 +11,15 @@ import java.util.List;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+
+    @Autowired
+    private MailSender mailSender;
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public String sendEmailToAllUsers(String message) {
+        List<User> users = userRepository.findAll();
+        return mailSender.send(users, message);
     }
 }
