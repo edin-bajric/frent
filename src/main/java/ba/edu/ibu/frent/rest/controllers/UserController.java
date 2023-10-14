90a16200013544d5931fcbe96d75bce67055da9b
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-10-14 13:51:42 +0200

    implemented user model, repo, service and controller

---
+++ src/main/java/ba/edu/ibu/frent/rest/controllers/UserController.java
@@ -0,0 +1,22 @@
+package ba.edu.ibu.frent.rest.controllers;
+
+import ba.edu.ibu.frent.core.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/send-to-all")
+    public String sendEmailToAllUsers(@RequestParam String message) {
+        return userService.sendEmailtoAllUsers(message);
+    }
+}
