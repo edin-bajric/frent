89044247b0fc3acf246ba77c388628e2780d49d7
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-10-19 19:53:40 +0200

    modified UserRepository (it's empty now, but it's an interface)

--- src/main/java/ba/edu/ibu/frent/core/repository/UserRepository.java
+++ src/main/java/ba/edu/ibu/frent/core/repository/UserRepository.java
@@ -1,27 +1,13 @@
 package ba.edu.ibu.frent.core.repository;
 
 import ba.edu.ibu.frent.core.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends MongoRepository<User, String> {
 
-    private List<User> users;
-
-    public UserRepository() {
-        this.users = Arrays.asList(
-                new User(1, "Edin", "Bajrić", "edin.bajric@stu.ibu.edu.ba"),
-                new User(2, "Amar", "Hodžić", "edin.bajric4321@gmail.com")
-        );
-    }
-    public List<User> findAll() {
-        return users;
-    }
-
-    public User findById(int id) {
-        return users.stream().filter(user -> user.getId() == id).findFirst().orElse(null);
-    }
 }
