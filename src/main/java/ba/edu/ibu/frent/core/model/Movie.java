d55007f3dda35367ee49abb87a78fa7370285ed5
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-10-20 16:49:03 +0200

    updated Movie model

--- src/main/java/ba/edu/ibu/frent/core/model/Movie.java
+++ src/main/java/ba/edu/ibu/frent/core/model/Movie.java
@@ -1,25 +1,23 @@
 package ba.edu.ibu.frent.core.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
 public class Movie {
-    private int id;
+    @Id
+    private String id;
     private String title;
     private String director;
     private String genre;
     private int year;
-
-    public Movie(int id, String title, String director, String genre, int year) {
-        this.id = id;
-        this.title = title;
-        this.director = director;
-        this.genre = genre;
-        this.year = year;
-    }
-
-    public int getId() {
+    private boolean available;
+    private double rentalPrice;
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -53,5 +51,21 @@ public class Movie {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public double getRentalPrice() {
+        return rentalPrice;
+    }
+
+    public void setRentalPrice(double rentalPrice) {
+        this.rentalPrice = rentalPrice;
     }
 }
