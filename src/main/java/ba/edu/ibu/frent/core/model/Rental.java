2aa81f19daf3ca10c5136574027397fb615ed5d4
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-10-20 19:50:31 +0200

    added Rental model

---
+++ src/main/java/ba/edu/ibu/frent/core/model/Rental.java
@@ -0,0 +1,65 @@
+package ba.edu.ibu.frent.core.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document
+public class Rental {
+    @Id
+    private String id;
+    private String userId;
+    private String movieId;
+    private Date rentalDate;
+    private Date returnDate;
+    private double rentalPrice;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public Date getRentalDate() {
+        return rentalDate;
+    }
+
+    public void setRentalDate(Date rentalDate) {
+        this.rentalDate = rentalDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public double getRentalPrice() {
+        return rentalPrice;
+    }
+
+    public void setRentalPrice(double rentalPrice) {
+        this.rentalPrice = rentalPrice;
+    }
+}
