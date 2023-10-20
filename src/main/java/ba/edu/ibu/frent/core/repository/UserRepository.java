77b61d93f7e05a35e81a308e5fbacf58bc859648
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-10-20 14:46:57 +0200

    updated UserService and UserRepository

--- src/main/java/ba/edu/ibu/frent/core/repository/UserRepository.java
+++ src/main/java/ba/edu/ibu/frent/core/repository/UserRepository.java
@@ -4,10 +4,9 @@ import ba.edu.ibu.frent.core.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-
+    Optional<User> findFirstByEmailLike(String emailPattern);
 }
