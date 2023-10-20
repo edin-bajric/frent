5a7f890d24de469381073db77a747ce595af2488
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-10-20 19:15:17 +0200

    updated MovieService

--- src/main/java/ba/edu/ibu/frent/core/service/MovieService.java
+++ src/main/java/ba/edu/ibu/frent/core/service/MovieService.java
@@ -1,10 +1,18 @@
 package ba.edu.ibu.frent.core.service;
 
+import ba.edu.ibu.frent.core.exceptions.repository.ResourceNotFoundException;
+import ba.edu.ibu.frent.core.model.Movie;
 import ba.edu.ibu.frent.core.model.Movie;
 import ba.edu.ibu.frent.core.repository.MovieRepository;
+import ba.edu.ibu.frent.rest.dto.MovieDTO;
+import ba.edu.ibu.frent.rest.dto.MovieDTO;
+import ba.edu.ibu.frent.rest.dto.MovieRequestDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 public class MovieService {
@@ -14,11 +22,41 @@ public class MovieService {
         this.movieRepository = movieRepository;
     }
 
-    public List<Movie> findAll() {
-        return movieRepository.findAll();
+    public List<MovieDTO> getMovies() {
+        List<Movie> movies = movieRepository.findAll();
+
+        return movies
+                .stream()
+                .map(MovieDTO::new)
+                .collect(toList());
     }
 
-    public Movie findById(int id) {
-        return movieRepository.findById(id);
+    public MovieDTO getMovieById(String id) {
+        Optional<Movie> movie = movieRepository.findById(id);
+        if (movie.isEmpty()) {
+            throw new ResourceNotFoundException("The movie with the given ID does not exist.");
+        }
+        return new MovieDTO(movie.get());
+    }
+
+    public MovieDTO addMovie(MovieRequestDTO payload) {
+        Movie movie = movieRepository.save(payload.toEntity());
+        return new MovieDTO(movie);
+    }
+
+    public MovieDTO updateMovie(String id, MovieRequestDTO payload) {
+        Optional<Movie> movie = movieRepository.findById(id);
+        if (movie.isEmpty()) {
+            throw new ResourceNotFoundException("The movie with the given ID does not exist.");
+        }
+        Movie updatedMovie = payload.toEntity();
+        updatedMovie.setId(movie.get().getId());
+        updatedMovie = movieRepository.save(updatedMovie);
+        return new MovieDTO(updatedMovie);
+    }
+
+    public void deleteMovie(String id) {
+        Optional<Movie> movie = movieRepository.findById(id);
+        movie.ifPresent(movieRepository::delete);
     }
 }
