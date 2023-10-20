b8498b667a2454fb2e13a93cce85786a3f7376d4
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-10-20 14:46:57 +0200

    updated UserService and UserRepository

--- src/main/java/ba/edu/ibu/frent/core/service/UserService.java
+++ src/main/java/ba/edu/ibu/frent/core/service/UserService.java
@@ -1,12 +1,18 @@
 package ba.edu.ibu.frent.core.service;
 
 import ba.edu.ibu.frent.core.api.mailsender.MailSender;
+import ba.edu.ibu.frent.core.exceptions.repository.ResourceNotFoundException;
 import ba.edu.ibu.frent.core.model.User;
 import ba.edu.ibu.frent.core.repository.UserRepository;
+import ba.edu.ibu.frent.rest.dto.UserDTO;
+import ba.edu.ibu.frent.rest.dto.UserRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 public class UserService {
@@ -20,16 +26,46 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public String sendEmailToAllUsers(String message) {
+    public List<UserDTO> getUsers() {
         List<User> users = userRepository.findAll();
-        return sendgridSender.send(users, message);
+
+        return users
+                .stream()
+                .map(UserDTO::new)
+                .collect(toList());
     }
 
-    public List<User> findAll() {
-        return userRepository.findAll();
+    public UserDTO getUserById(String id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            throw new ResourceNotFoundException("The user with the given ID does not exist.");
+        }
+        return new UserDTO(user.get());
     }
 
-    public User findById(int id) {
-        return userRepository.findById(id);
+    public UserDTO addUser(UserRequestDTO payload) {
+        User user = userRepository.save(payload.toEntity());
+        return new UserDTO(user);
+    }
+
+    public UserDTO updateUser(String id, UserRequestDTO payload) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            throw new ResourceNotFoundException("The user with the given ID does not exist.");
+        }
+        User updatedUser = payload.toEntity();
+        updatedUser.setId(user.get().getId());
+        updatedUser = userRepository.save(updatedUser);
+        return new UserDTO(updatedUser);
+    }
+
+    public void deleteUser(String id) {
+        Optional<User> user = userRepository.findById(id);
+        user.ifPresent(userRepository::delete);
+    }
+
+    public UserDTO filterByEmail(String email) {
+        Optional<User> user = userRepository.findFirstByEmailLike(email);
+        return user.map(UserDTO::new).orElse(null);
     }
 }
