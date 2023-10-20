4ab3a8a60336dfd03bbaabf2322b842dd44bfedd
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-10-20 20:08:53 +0200

    added RentalDTO

---
+++ src/main/java/ba/edu/ibu/frent/rest/dto/RentalDTO.java
@@ -0,0 +1,71 @@
+package ba.edu.ibu.frent.rest.dto;
+
+import ba.edu.ibu.frent.core.model.Rental;
+
+import java.util.Date;
+
+public class RentalDTO {
+    private String id;
+    private String userId;
+    private String movieId;
+    private Date rentalDate;
+    private Date returnDate;
+    private double rentalPrice;
+
+    public RentalDTO(Rental rental) {
+        this.id = rental.getId();
+        this.userId = rental.getUserId();
+        this.movieId = rental.getMovieId();
+        this.rentalDate = rental.getRentalDate();
+        this.returnDate = rental.getReturnDate();
+        this.rentalPrice = rental.getRentalPrice();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public Date getRentalDate() {
+        return rentalDate;
+    }
+
+    public void setRentalDate(Date rentalDate) {
+        this.rentalDate = rentalDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public double getRentalPrice() {
+        return rentalPrice;
+    }
+
+    public void setRentalPrice(double rentalPrice) {
+        this.rentalPrice = rentalPrice;
+    }
+}
