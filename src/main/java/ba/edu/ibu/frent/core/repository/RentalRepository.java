beca9e39052b92f44258acbb989a952802ec362d
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-10-20 19:51:47 +0200

    added RentalRepository

---
+++ src/main/java/ba/edu/ibu/frent/core/repository/RentalRepository.java
@@ -0,0 +1,9 @@
+package ba.edu.ibu.frent.core.repository;
+
+import ba.edu.ibu.frent.core.model.Rental;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RentalRepository extends MongoRepository<Rental, String> {
+}
