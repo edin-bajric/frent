23e35f3c0c4ad9056079840cbd4caaee621811b9
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-10-23 17:30:12 +0200

    added movie genre as enum, fixed small error with Sendgrid

---
+++ src/main/java/ba/edu/ibu/frent/core/model/enums/Genre.java
@@ -0,0 +1,6 @@
+package ba.edu.ibu.frent.core.model.enums;
+
+public enum Genre {
+
+    HORROR, DRAMA, CRIME, ADVENTURE, THRILLER, ACTION, SCIENCE_FICTION, ANIMATION, COMEDY, MYSTERY, FANTASY, WESTERN
+}
