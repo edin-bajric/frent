b0a77982c958976069ff42730950e373b608f80d
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-11-17 23:55:27 +0100

    removed unused import

--- src/test/java/ba/edu/ibu/frent/core/repository/RentalRepositoryTest.java
+++ src/test/java/ba/edu/ibu/frent/core/repository/RentalRepositoryTest.java
@@ -1,6 +1,5 @@
 package ba.edu.ibu.frent.core.repository;
 
-import ba.edu.ibu.frent.core.model.Movie;
 import ba.edu.ibu.frent.core.model.Rental;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
