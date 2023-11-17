bda0a1f4c0641a6cbfd60c53085f0ba0f4dc6e07
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-11-17 18:28:37 +0100

    rentalDate auto assign on creation, returnDate and returned assigned upon return, all removed from RentalRequestDTO

--- src/main/java/ba/edu/ibu/frent/rest/dto/RentalRequestDTO.java
+++ src/main/java/ba/edu/ibu/frent/rest/dto/RentalRequestDTO.java
@@ -7,30 +7,24 @@ import java.time.LocalDate;
 public class RentalRequestDTO {
     private String userId;
     private String movieId;
-    private LocalDate rentalDate;
-    private LocalDate returnDate;
     private double rentalPrice;
-    private boolean returned;
 
     public RentalRequestDTO() { }
 
     public RentalRequestDTO(Rental rental) {
         this.userId = rental.getUserId();
         this.movieId = rental.getMovieId();
-        this.rentalDate = rental.getRentalDate();
-        this.returnDate = rental.getReturnDate();
         this.rentalPrice = rental.getRentalPrice();
-        this.returned = rental.isReturned();
     }
 
     public Rental toEntity() {
         Rental rental = new Rental();
         rental.setUserId(userId);
         rental.setMovieId(movieId);
-        rental.setRentalDate(rentalDate);
-        rental.setReturnDate(returnDate);
+        rental.setRentalDate(LocalDate.now());
+        rental.setReturnDate(null);
         rental.setRentalPrice(rentalPrice);
-        rental.setReturned(returned);
+        rental.setReturned(false);
         return rental;
     }
 
@@ -50,35 +44,11 @@ public class RentalRequestDTO {
         this.movieId = movieId;
     }
 
-    public LocalDate getRentalDate() {
-        return rentalDate;
-    }
-
-    public void setRentalDate(LocalDate rentalDate) {
-        this.rentalDate = rentalDate;
-    }
-
-    public LocalDate getReturnDate() {
-        return returnDate;
-    }
-
-    public void setReturnDate(LocalDate returnDate) {
-        this.returnDate = returnDate;
-    }
-
     public double getRentalPrice() {
         return rentalPrice;
     }
 
     public void setRentalPrice(double rentalPrice) {
         this.rentalPrice = rentalPrice;
-    }
-
-    public boolean isReturned() {
-        return returned;
-    }
-
-    public void setReturned(boolean returned) {
-        this.returned = returned;
     }
 }
