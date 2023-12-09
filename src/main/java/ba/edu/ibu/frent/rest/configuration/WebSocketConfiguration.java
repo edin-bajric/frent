2eb31a0f3981d13fe2453f09e1eb68aff8738d19
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2023-12-09 14:54:57 +0100

    added WebSocketConfiguration

---
+++ src/main/java/ba/edu/ibu/frent/rest/configuration/WebSocketConfiguration.java
@@ -0,0 +1,19 @@
+package ba.edu.ibu.frent.rest.configuration;
+
+import ba.edu.ibu.frent.rest.websockets.MainSocketHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+    @Autowired
+    MainSocketHandler mainSocketHandler;
+
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(mainSocketHandler, "/websocket").setAllowedOrigins("*");
+    }
+}
