84374b9a903e25dc13f260296357622800e37a0f
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-01-12 17:51:23 +0100

    added simple routing

--- src/main/resources/static/ui/frent-ui/src/App.tsx
+++ src/main/resources/static/ui/frent-ui/src/App.tsx
@@ -1,6 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import { movie1, movie2, notification1, notification2 } from "./constants";
+import { Home, Movies, Rentals, SignIn, Register, NotFound } from "./pages";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   const movies = [movie1, movie2];
@@ -8,6 +10,15 @@ export default function App() {
   return (
     <>
       <Navbar movies={movies} notifications={notifications} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/rentals" element={<Rentals />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
