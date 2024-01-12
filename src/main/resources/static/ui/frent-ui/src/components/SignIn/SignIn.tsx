64b130aea16ed71ba1a7d0ba3ca13dc785246473
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-01-12 01:55:47 +0100

    now sing in and register are their own pages

--- src/main/resources/static/ui/frent-ui/src/components/SignIn/SignIn.tsx
+++ src/main/resources/static/ui/frent-ui/src/components/SignIn/SignIn.tsx
@@ -1,25 +1,25 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "../../assets/css/SignIn.css";
 
 function BasicExample() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Sign in
-      </Button>
-    </Form>
+    <div className="full-page-container">
+      <div className="centered-form-container">
+        <h1 className="title">Sign in</h1>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="Email" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Sign in
+          </Button>
+        </Form>
+      </div>
+    </div>
   );
 }
 
