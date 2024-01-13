d0d9901fd455ca372cee8005e3fef81c5a9b9bd5
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-01-13 23:26:53 +0100

    changed the look of the spinner

--- src/main/resources/static/ui/frent-ui/src/components/Spinner/Spinner.tsx
+++ src/main/resources/static/ui/frent-ui/src/components/Spinner/Spinner.tsx
@@ -3,7 +3,7 @@ import Spinner from 'react-bootstrap/Spinner';
 function BasicExample() {
   return (
     <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
-      <Spinner animation="border" role="status" variant="primary">
+      <Spinner animation="grow" role="status" variant="primary">
         <span className="visually-hidden">Loading...</span>
       </Spinner>
     </div>
