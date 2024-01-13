43a1e9f7a64804acc8680aa1d75c3d8009f22ee0
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-01-13 23:04:31 +0100

    added other CRUD operations to movieService

--- src/main/resources/static/ui/frent-ui/src/services/movies.ts
+++ src/main/resources/static/ui/frent-ui/src/services/movies.ts
@@ -5,10 +5,36 @@ const getMovies = async (): Promise<Movie[]> => {
     return appAxios.get('/movies/').then(
         (response) => {
             const data = response.data;
-            console.log(data);
             return data;
         }
     );
 }
 
-export default { getMovies };
+const addMovie = async (movie: Movie): Promise<Movie> => {
+    return appAxios.post('/movies/', movie).then(
+        (response) => {
+            const data = response.data;
+            return data;
+        }
+    );
+}
+
+const deleteMovie = async (id: number): Promise<Movie> => {
+    return appAxios.delete(`/movies/${id}`).then(
+        (response) => {
+            const data = response.data;
+            return data;
+        }
+    );
+}
+
+const updateMovie = async (movie: Movie): Promise<Movie> => {
+    return appAxios.put(`/movies/${movie.id}`, movie).then(
+        (response) => {
+            const data = response.data;
+            return data;
+        }
+    );
+}
+
+export default { getMovies, addMovie, deleteMovie, updateMovie };
