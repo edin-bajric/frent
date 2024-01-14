5521597aa0addbb0cb0c352bdfb3f2edb9184de2
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-01-14 19:13:47 +0100

    added protected routes

--- src/main/resources/static/ui/frent-ui/src/App.tsx
+++ src/main/resources/static/ui/frent-ui/src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import { movie1, movie2, notification1, notification2 } from "./constants";
 import { Home, Movies, Rentals, SignInPage, RegisterPage, NotFound, SearchResult } from "./pages";
 import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./utils/ProtectedRoute";
 
 export default function App() {
   const movies = [movie1, movie2];
@@ -14,7 +15,9 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
+        <Route element={<ProtectedRoute />}>
         <Route path="/rentals" element={<Rentals />} />
+        </Route>
         <Route path="/login" element={<SignInPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/search" element={<SearchResult />} />
--- src/main/resources/static/ui/frent-ui/src/components/SignIn/SignIn.tsx
+++ src/main/resources/static/ui/frent-ui/src/components/SignIn/SignIn.tsx
@@ -3,7 +3,7 @@ import Form from "react-bootstrap/Form";
 import "../../assets/css/SingInAndRegister.css";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from 'yup';
+import * as yup from "yup";
 import { login } from "../../store/authSlice";
 import LoadingButton from "../LoadingButton";
 import { useEffect } from "react";
@@ -16,17 +16,28 @@ export type SignInFormData = {
   password: string;
 };
 
-const schema = yup.object({
-  email: yup.string().email("Invalid email.").required("Email is required."),
-  password: yup.string().min(8, "Password must be at least 8 characters.").required()
-}).required();
+const schema = yup
+  .object({
+    email: yup.string().email("Invalid email.").required("Email is required."),
+    password: yup
+      .string()
+      .min(8, "Password must be at least 8 characters.")
+      .required(),
+  })
+  .required();
 
 function BasicExample() {
-  const { register, handleSubmit, formState: { errors } } = useForm<SignInFormData>({
-    resolver: yupResolver(schema)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInFormData>({
+    resolver: yupResolver(schema),
   });
 
-  const { loading, userToken, error } = useSelector((state: RootState) => state.auth);
+  const { loading, userToken, error } = useSelector(
+    (state: RootState) => state.auth
+  );
 
   const dispatch = useDispatch<AppDispatch>();
 
@@ -34,12 +45,12 @@ function BasicExample() {
 
   useEffect(() => {
     if (userToken) {
-      navigate('/home')
+      navigate("/home");
     }
   }, [navigate, userToken]);
 
   const onSubmit = (data: SignInFormData) => {
-    dispatch(login(data))
+    dispatch(login(data));
   };
 
   return (
@@ -48,15 +59,27 @@ function BasicExample() {
         <h1 className="title">Log in</h1>
         <Form onSubmit={handleSubmit(onSubmit)}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Email" {...register("email")}/>
-            {errors.email && <p className="text-danger">{errors.email.message}</p>}
+            <Form.Control
+              type="email"
+              placeholder="Email"
+              {...register("email")}
+            />
+            {errors.email && (
+              <p className="text-danger">{errors.email.message}</p>
+            )}
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Password" {...register("password")}/>
-            {errors.password && <p className="text-danger">{errors.password.message}</p>}
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              {...register("password")}
+            />
+            {errors.password && (
+              <p className="text-danger">{errors.password.message}</p>
+            )}
           </Form.Group>
           <Button variant="primary" type="submit" disabled={loading}>
-            {loading ? <LoadingButton/> : "Log in"}
+            {loading ? <LoadingButton /> : "Log in"}
           </Button>
         </Form>
         {error && <span className="text-danger">{error}</span>}
