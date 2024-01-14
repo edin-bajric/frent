1faee1acca02327b2d3ae0cb84ecb9492f5ad8f4
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-01-14 15:50:38 +0100

    modified movie fetching to use react query and added a custom error component

--- src/main/resources/static/ui/frent-ui/src/main.tsx
+++ src/main/resources/static/ui/frent-ui/src/main.tsx
@@ -15,7 +15,6 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
       <BrowserRouter>
         <App />
       </BrowserRouter>
