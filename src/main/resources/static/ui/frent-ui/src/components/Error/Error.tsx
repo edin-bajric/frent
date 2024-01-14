0bd1aeb8223d6b07ab202e56c1c28915766a2a87
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-01-14 15:50:38 +0100

    modified movie fetching to use react query and added a custom error component

--- src/main/resources/static/ui/frent-ui/src/components/Error/Error.tsx
+++ src/main/resources/static/ui/frent-ui/src/components/Error/Error.tsx
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
 
-const NotFound = () => {
+function BasicExample() {
   return (
     <div className="d-flex align-items-center justify-content-center vh-100">
       <div className="text-center">
-        <h1 className="display-1 fw-bold">404</h1>
+        <h1 className="display-1 fw-bold">Error</h1>
         <p className="fs-3">
           {" "}
-          <span className="text-danger">Oops!</span> Page not found.
+          <span className="text-danger">Oops!</span> Something went wrong.
         </p>
-        <p className="lead">The page you’re looking for doesn’t exist.</p>
+        <p className="lead">Please try again, or</p>
         <Link to="/home" className="btn btn-primary">
           Go Home
         </Link>
       </div>
     </div>
   );
-};
+}
 
-export default NotFound;
+export default BasicExample;
--- src/main/resources/static/ui/frent-ui/src/components/MovieGrid/MovieGrid.tsx
+++ src/main/resources/static/ui/frent-ui/src/components/MovieGrid/MovieGrid.tsx
@@ -1,44 +1,17 @@
-import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import MovieCard from "../MovieCard";
-import { Movie } from "../../utils/types";
-import { MovieService } from "../../services";
 import Spinner from "../Spinner";
+import useMovies from "../../hooks/useMovies";
+import Error from "../Error";
 
 const MoviesGrid = () => {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [loading, setIsLoading] = useState(true);
-  const [error, setError] = useState<any>();
+  const { data: movies, error, isLoading, isError } = useMovies();
 
-  useEffect(() => {
-    setIsLoading(true);
-    MovieService.getMovies()
-      .then((data) => {
-        setMovies(data);
-      })
-      .catch((error) => {
-        setError(error);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []);
   return (
     <>
-      {loading && <Spinner />}
-      {error && (
-        <div className="row">
-          <div className="col-12 col-md-2 m-3">
-            <div className="alert alert-danger" role="alert">
-              <h4 className="alert-heading">Unable to render data!</h4>
-              <p>{error?.response?.data?.message || error?.message}</p>
-              <hr />
-              <p className="mb-0">Something went wrong, please try again.</p>
-            </div>
-          </div>
-        </div>
-      )}
-      {!loading && (
+      {isLoading && <Spinner />}
+      {error && <Error />}
+      {!isLoading && !isError && (
         <Row
           xs={1}
           md={2}
@@ -46,9 +19,9 @@ const MoviesGrid = () => {
           xl={4}
           xxl={5}
           className="g-4"
-          style={{ width: "100%", padding: "16px"}}
+          style={{ width: "100%", padding: "16px" }}
         >
-          {movies.map((movie, i) => (
+          {movies?.map((movie, i) => (
             <Col key={i}>
               <MovieCard movie={movie} />
             </Col>
