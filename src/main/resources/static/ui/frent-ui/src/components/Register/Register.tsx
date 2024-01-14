e3a6d33281d8c2d8387b6b378ec3a6ed0efcc5c7
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-01-14 17:08:13 +0100

    added react-hook-form and registration logs to the console for now

--- src/main/resources/static/ui/frent-ui/src/components/Register/Register.tsx
+++ src/main/resources/static/ui/frent-ui/src/components/Register/Register.tsx
@@ -1,25 +1,40 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "../../assets/css/SingInAndRegister.css";
+import { useForm } from "react-hook-form";
+
+export type RegisterFormData = {
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  password: string;
+};
 
 function BasicExample() {
+  const { register, handleSubmit, formState: { errors } } = useForm<RegisterFormData>();
+
+  const onSubmit = (data: RegisterFormData) => {
+    console.log(data);
+  };
+
   return (
     <div className="full-page-container">
       <div className="centered-form-container">
         <h1 className="title">Register</h1>
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-3 d-flex">
-            <Form.Control type="text" placeholder="First name" className="me-2" />
-            <Form.Control type="text" placeholder="Last name" />
+            <Form.Control type="text" placeholder="First name" className="me-2" {...register("firstName")}/>
+            <Form.Control type="text" placeholder="Last name" {...register("lastName")}/>
           </div>
           <Form.Group className="mb-3">
-            <Form.Control type="text" placeholder="Username" />
+            <Form.Control type="text" placeholder="Username" {...register("username")}/>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Email" />
+            <Form.Control type="email" placeholder="Email" {...register("email")}/>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" placeholder="Password" {...register("password")}/>
           </Form.Group>
           <Button variant="primary" type="submit">
             Register
