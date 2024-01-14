783ba9ecd993e33d6892d974af2719d2a8f94f65
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-01-14 14:44:13 +0100

    added query client and query client provider to main.tsx

--- src/main/resources/static/ui/frent-ui/src/main.tsx
+++ src/main/resources/static/ui/frent-ui/src/main.tsx
@@ -2,11 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
       <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
