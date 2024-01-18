2a88352046f069b3b80fe74ec2129ac430525920
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-01-18 01:32:22 +0100

    adjusted the carousel change intervals

--- src/main/resources/static/ui/frent-ui/src/components/Carousel/Carousel.tsx
+++ src/main/resources/static/ui/frent-ui/src/components/Carousel/Carousel.tsx
@@ -7,7 +7,7 @@ import "../../assets/css/Carousel.css";
 function UncontrolledExample() {
   return (
     <Carousel className="carousel">
-      <Carousel.Item>
+      <Carousel.Item interval={3000}>
         <img
           src={browseImage}
           className="carousel-img"
@@ -18,14 +18,14 @@ function UncontrolledExample() {
           <p className="carousel-text">Discover, Explore, Find Your Flick!</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item interval={3000}>
         <img src={rentImage} className="carousel-img" alt="Rent Image"></img>
         <Carousel.Caption>
           <h3 className="carousel-text">Rent</h3>
           <p className="carousel-text">Grab the Popcorn, Rent the Fun!</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item interval={3000}>
         <img src={enjoyImage} className="carousel-img" alt="Enjoy Image"></img>
         <Carousel.Caption>
           <h3 className="carousel-text">Enjoy</h3>
