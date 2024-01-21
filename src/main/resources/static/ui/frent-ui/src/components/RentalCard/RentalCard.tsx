d387e057f0fbbe04e85753eee2289ccec722bc6c
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-01-21 16:39:19 +0100

    added rental info

--- src/main/resources/static/ui/frent-ui/src/components/RentalCard/RentalCard.tsx
+++ src/main/resources/static/ui/frent-ui/src/components/RentalCard/RentalCard.tsx
@@ -5,6 +5,7 @@ import Badge from "react-bootstrap/Badge";
 import { RentalMovie } from "../../utils/types";
 import "../../assets/css/MovieRentalCard.css";
 import useReturnRentalForUser from "../../hooks/useReturnRentals";
+import RentalInfo from "../RentalInfo";
 
 type Props = {
   rentalMovie: RentalMovie;
@@ -12,6 +13,7 @@ type Props = {
 
 const BasicExample = ({ rentalMovie }: Props) => {
   const [isReturned, setIsReturned] = useState(rentalMovie.returned);
+  const [showRentalInfo, setShowRentalInfo] = useState(false);
   const returnRentalMutation = useReturnRentalForUser();
 
   const handleReturnClick = () => {
@@ -24,34 +26,47 @@ const BasicExample = ({ rentalMovie }: Props) => {
     });
   };
 
+  const handleRentalCardClick = () => {
+    setShowRentalInfo(true);
+  };
+
   return (
-    <Card style={{ width: "18rem" }} bg="dark" text="light">
-      <Card.Img
-        variant="top"
-        src={rentalMovie.smallImage}
-        style={{ height: "18rem", objectFit: "cover" }}
+    <>
+      <Card style={{ width: "18rem" }} bg="dark" text="light">
+        <Card.Img
+          variant="top"
+          src={rentalMovie.smallImage}
+          style={{ height: "18rem", objectFit: "cover", cursor: "pointer" }}
+          onClick={handleRentalCardClick}
+        />
+        <Card.Body>
+          <Card.Title as="h6">{rentalMovie.title}</Card.Title>
+          <Card.Text style={{ color: "crimson" }} as="h5">
+            Valid until {rentalMovie.dueDate.toString()}
+          </Card.Text>
+          <Badge bg="secondary" style={{ marginBottom: "8px" }}>
+            {isReturned ? "Returned" : "Rented"}
+          </Badge>
+          <Card.Text className="clamp-two-lines">
+            {rentalMovie.description}
+          </Card.Text>
+          <Button
+            variant="primary"
+            style={{ marginRight: "8px" }}
+            onClick={handleReturnClick}
+            disabled={isReturned}
+          >
+            Return
+          </Button>
+        </Card.Body>
+      </Card>
+      <RentalInfo
+        show={showRentalInfo}
+        handleClose={() => setShowRentalInfo(false)}
+        rentalMovie={rentalMovie}
+        isReturned={isReturned}
       />
-      <Card.Body>
-        <Card.Title as="h6">{rentalMovie.title}</Card.Title>
-        <Card.Text style={{ color: "crimson" }} as="h5">
-          Valid until {rentalMovie.dueDate.toString()}
-        </Card.Text>
-        <Badge bg="secondary" style={{ marginBottom: "8px" }}>
-          {isReturned ? "Returned" : "Rented"}
-        </Badge>
-        <Card.Text className="clamp-two-lines">
-          {rentalMovie.description}
-        </Card.Text>
-        <Button
-          variant="primary"
-          style={{ marginRight: "8px" }}
-          onClick={handleReturnClick}
-          disabled={isReturned}
-        >
-          Return
-        </Button>
-      </Card.Body>
-    </Card>
+    </>
   );
 };
 
