2a4e56eca222ad048a37d433d64ed9bea45df1cb
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-04-22 03:13:39 +0200

    fixed genre not displaying properly

--- src/main/resources/static/ui/frent-ui/src/utils/types.ts
+++ src/main/resources/static/ui/frent-ui/src/utils/types.ts
@@ -5,10 +5,17 @@ export type Movie = {
   smallImage: string;
   bigImage: string;
   director: string;
-  genre: string;
+  genre: string[];
   year: number;
   available: boolean;
   rentalPrice: number;
+};
+
+export const joinGenres = (genres: string[]): string => {
+  return genres.map(genre => {
+    const capitalizedParts = genre.split('_').map(part => part.charAt(0).toUpperCase() + part.slice(1).toLowerCase());
+    return capitalizedParts.join(' ');
+  }).join(', ');
 };
 
 export type Rental = {
