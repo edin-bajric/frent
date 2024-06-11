8d2002bb5595afce274adc2b0c42455eee74846d
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-06-11 18:34:56 +0200

    HashRouter test

--- src/main/resources/static/ui/frent-ui/src/main.tsx
+++ src/main/resources/static/ui/frent-ui/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -17,11 +17,11 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <Provider store={store}>
           <App />
         </Provider>
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </React.StrictMode>
 );
