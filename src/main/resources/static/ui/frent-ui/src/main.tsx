4aedfccaac3a49500486806770b9effa7aec9380
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-06-11 18:48:06 +0200

    reverted back to BrowserRouter

--- src/main/resources/static/ui/frent-ui/src/main.tsx
+++ src/main/resources/static/ui/frent-ui/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -17,11 +17,11 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <HashRouter>
+      <BrowserRouter>
         <Provider store={store}>
           <App />
         </Provider>
-      </HashRouter>
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
 );
