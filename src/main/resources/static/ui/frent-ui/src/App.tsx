33adb94b7582bd24713df0c8a511684dc56e6655
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-06-11 15:48:26 +0200

    again using reagular router

--- src/main/resources/static/ui/frent-ui/src/App.tsx
+++ src/main/resources/static/ui/frent-ui/src/App.tsx
@@ -10,7 +10,7 @@ import {
   Search,
   AdminPanel,
 } from "./pages";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./utils/ProtectedRoute";
 import AdminProtectedRoute from "./utils/AdminProtectedRoute";
 import { useDispatch } from "react-redux";
@@ -27,23 +27,21 @@ export default function App() {
   return (
     <>
       <Navbar />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/rentals" element={<Rentals />} />
-          </Route>
-          <Route path="/login" element={<SignInPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/search/:keyword/:page/:size" element={<Search />} />
-          <Route element={<AdminProtectedRoute />}>
-            <Route path="/dashboard" element={<AdminPanel />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/rentals" element={<Rentals />} />
+        </Route>
+        <Route path="/login" element={<SignInPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/search/:keyword/:page/:size" element={<Search />} />
+        <Route element={<AdminProtectedRoute />}>
+          <Route path="/dashboard" element={<AdminPanel />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
